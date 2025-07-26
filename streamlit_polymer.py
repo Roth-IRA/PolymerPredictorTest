76de3f7ce9ee47a23e143f68fa22d15badeea39c
Roth-IRA <crothro3@charlotte.edu>
2025-07-26 13:19:23 -0400

    Update streamlit_polymer.py

--- streamlit_polymer.py
+++ streamlit_polymer.py
@@ -29,9 +29,9 @@ def smiles_to_fp(smiles, radius=4):
 
 user_input = smiles_to_fp(user_input)
 if user_input is not None:
-    st.write("Predicted Chem Properties: "
+    st.write("Predicted Chem Properties: ")
     for key in model:
         prediction = model[key].predict(user_input)
-        st.write(key," ", prediction)
+        st.write(key,": ", prediction)
 else:
     st.write("Please input a rdkit-recognized chemical structure!")
