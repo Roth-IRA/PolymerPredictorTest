fc24bd1f1cf37edf3b5a67312f4ae36c721d0336
Roth-IRA <crothro3@charlotte.edu>
2025-07-26 14:47:39 -0400

    Update streamlit_polymer.py

--- streamlit_polymer.py
+++ streamlit_polymer.py
@@ -7,7 +7,7 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split, KFold
 from xgboost import XGBRegressor # opted for XGBoost since it routinely outperforms most other algorithms in kaggle competitions
 from sklearn.metrics import mean_absolute_error # the competition uses a weighted MAE since it involves making predictions for multiple numeric variables
-from rdkit import Chem
+from rdkit import Chem, DataStructs
 from rdkit.Chem import Descriptors
 from rdkit.Chem import AllChem
 from sklearn.impute import KNNImputer
@@ -37,17 +37,24 @@ st.title("Polymer Prediction, Carl R.")
 st.write("### Input valid SMILES notation for a chemical species to get chemical property predictions!")
 user_input = st.text_input("SMILES:")
 
-def smiles_to_fp(smiles, radius=4):
+def smiles_to_fp(smiles, radius=4, n_bits = 2048):
     mol = Chem.MolFromSmiles(smiles) # this creates a molecule object from SMILES notation (used to make sure the polymer is valid)
     if mol is not None:
         # SMILES ("Simplified Molecular Input Line Entry System") is notation for describing the structure of a polymer
         # MorganGenerator transforms SMILES from text to numbers
-        generator = AllChem.GetMorganGenerator(radius=radius)
-        return np.array(generator.GetFingerprint(mol))
+         # The fingerprint is a binary array which identifies which features a polymer does and does not have
+        bit_vect = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits = 2048)
+        arr = np.zeros((n_bits,),dtype=np.unit8)
+        DataStructs.ConvertToNumpyArray(bit_vect,arr)
+        return arr
     else:
         return None # Returns None for invalid SMILES
 
 user_input_fp = smiles_to_fp(user_input)
+
+if user_input_fp is not None:
+    user_input_fp = user_input_fp.reshape(1,-1)
+    
 if st.button("Predict Properties") and user_input_fp is not None:
     st.write("Predicted Chem Properties: ")
     
